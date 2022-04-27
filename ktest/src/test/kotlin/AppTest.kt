e3df30a56ba4741d41f1554f7df65b1eb82c366a
haliphax <haliphax@gmail.com>
2022-04-27 09:38:05 -0500

    🔥 clean up imports in test class

--- ktest/src/test/kotlin/AppTest.kt
+++ ktest/src/test/kotlin/AppTest.kt
@@ -1,18 +1,10 @@
 // 3rd party
-import io.ktor.client.*
 import io.ktor.client.call.*
-import io.ktor.client.engine.cio.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.request.*
-import io.ktor.client.statement.*
-import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
-import io.ktor.server.application.*
 import io.ktor.server.testing.*
 import kotlin.test.*
-import kotlinx.coroutines.*
-import kotlinx.serialization.*
-import kotlinx.serialization.json.*
 // local
 import com.haliphax.ktest.data.*
 
