c80b94e5df73fe215557124f213869b2e1db4f57
haliphax <haliphax@gmail.com>
2022-09-14 14:37:03 -0500

    👷 set java compatibility version

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 val kotlin_version: String by project
 
 plugins {
+	java
 	id("com.github.johnrengelman.shadow") apply false
 	id("org.jlleitschuh.gradle.ktlint") version "11.0.0"
 	id("org.jetbrains.kotlin.jvm")
@@ -10,9 +11,20 @@ plugins {
 }
 
 allprojects {
+	apply(plugin = "jacoco")
+	apply(plugin = "java")
+	apply(plugin = "com.github.johnrengelman.shadow")
+	apply(plugin = "org.jlleitschuh.gradle.ktlint")
+	apply(plugin = "org.jetbrains.kotlin.jvm")
+
 	repositories {
 		mavenCentral()
 	}
+
+	java {
+		sourceCompatibility = JavaVersion.VERSION_17
+	}
+
 	tasks.withType<KotlinCompile> {
 		kotlinOptions.jvmTarget = "1.8"
 	}
@@ -20,9 +32,5 @@ allprojects {
 
 subprojects {
 	apply(plugin = "application")
-	apply(plugin = "jacoco")
-	apply(plugin = "com.github.johnrengelman.shadow")
-	apply(plugin = "org.jlleitschuh.gradle.ktlint")
-	apply(plugin = "org.jetbrains.kotlin.jvm")
 	apply(plugin = "org.jetbrains.kotlin.plugin.serialization")
 }
