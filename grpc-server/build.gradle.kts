acfdb660b04e269f1e8c62542b2d11b3e268d026
haliphax <haliphax@gmail.com>
2022-10-04 22:51:50 -0500

    👷 build reorg: move protobuf to root, ignore generated source in ktlintcheck

--- grpc-server/build.gradle.kts
+++ grpc-server/build.gradle.kts
@@ -1,18 +1,3 @@
-import com.google.protobuf.gradle.generateProtoTasks
-import com.google.protobuf.gradle.id
-import com.google.protobuf.gradle.ofSourceSet
-import com.google.protobuf.gradle.plugins
-import com.google.protobuf.gradle.protobuf
-import com.google.protobuf.gradle.protoc
-
-val grpcVersion: String by project
-val grpcKotlinVersion: String by project
-val protobufVersion: String by project
-
-plugins {
-  id("com.google.protobuf")
-}
-
 dependencies {
   implementation(common.bundles.all)
   implementation(proto.bundles.all)
@@ -24,30 +9,6 @@ dependencies {
 
 application {
   mainClass.set("dev.haliphax.ktorStarterProject.MainKt")
-}
-
-protobuf {
-  protoc {
-    artifact = "com.google.protobuf:protoc:$protobufVersion"
-  }
-
-  plugins {
-    id("grpc") {
-      artifact = "io.grpc:protoc-gen-grpc-java:$grpcVersion"
-    }
-    id("grpcKt") {
-      artifact = "io.grpc:protoc-gen-grpc-kotlin:$grpcKotlinVersion:jdk7@jar"
-    }
-  }
-
-  generateProtoTasks {
-    ofSourceSet("main").forEach {
-      it.plugins {
-        id("grpc")
-        id("grpcKt")
-      }
-    }
-  }
 }
 
 tasks.jar {
