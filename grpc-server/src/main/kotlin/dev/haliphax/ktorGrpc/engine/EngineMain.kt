156f8d22e926f50735afc53bd28bb6a81412e2a4
haliphax <haliphax@gmail.com>
2022-10-05 02:33:17 -0500

    ♻️ move module loader into EngineMain

--- grpc-server/src/main/kotlin/dev/haliphax/ktorGrpc/engine/EngineMain.kt
+++ grpc-server/src/main/kotlin/dev/haliphax/ktorGrpc/engine/EngineMain.kt
@@ -1,7 +1,6 @@
 package dev.haliphax.ktorGrpc.engine
 
 import dev.haliphax.ktorGrpc.logging.HasLog
-import dev.haliphax.ktorGrpc.reflection.toCallables
 import io.ktor.server.application.Application
 import io.ktor.server.config.ApplicationConfig
 import io.ktor.server.engine.ApplicationEngineEnvironment
@@ -9,18 +8,29 @@ import io.ktor.server.engine.addShutdownHook
 import io.ktor.server.engine.applicationEngineEnvironment
 import org.koin.dsl.koinApplication
 
+typealias Module = Application.() -> Unit
+
 object EngineMain : HasLog {
-  private fun loadEnvironment(
-    loadedModules: MutableMap<String, Application.() -> Unit>
-  ): ApplicationEngineEnvironment =
+  private lateinit var environment: ApplicationEngineEnvironment
+
+  private fun mappedModules(modules: List<String>): Map<String, Module> =
+    modules.associate { fqn ->
+      val dotIndex = fqn.lastIndexOf('.')
+      val className = fqn.substring(0, dotIndex)
+      val methodName = fqn.substring(dotIndex + 1)
+      val klass = environment.classLoader.loadClass(className)
+
+      @Suppress("UNCHECKED_CAST")
+      Pair(fqn, { klass.methods.first { it.name == methodName } } as Module)
+    }
+
+  private fun loadEnvironment(modules: MutableMap<String, Module>): ApplicationEngineEnvironment =
     applicationEngineEnvironment {
       config = ApplicationConfig(null)
       this.modules.addAll(
         config.propertyOrNull("ktor.application.modules")?.let {
-          it.getList().toCallables(classLoader).map { loaded ->
-            loaded.toModule().apply {
-              loadedModules[loaded.fullyQualifiedName] = this
-            }
+          mappedModules(it.getList()).map { module ->
+            module.value.apply { modules[module.key] = this }
           }
         } ?: emptyList()
       )
@@ -31,8 +41,8 @@ object EngineMain : HasLog {
     args: Array<String>
   ) {
     koinApplication {
-      val modules = mutableMapOf<String, Application.() -> Unit>()
-      val environment = loadEnvironment(modules)
+      val modules = mutableMapOf<String, Module>()
+      environment = loadEnvironment(modules)
       val engine = GrpcApplicationEngine(environment)
 
       modules.forEach {
