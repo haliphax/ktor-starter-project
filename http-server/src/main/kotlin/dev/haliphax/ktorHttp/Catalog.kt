e3531398ab257022578f459b417db5565d7a6050
haliphax <haliphax@gmail.com>
2022-10-07 22:06:20 -0500

    🔥 eliminate need for catalog

--- http-server/src/main/kotlin/dev/haliphax/ktorHttp/Catalog.kt
+++
@@ -1,7 +0,0 @@
-package dev.haliphax.ktorHttp
-
-import dev.haliphax.ktorGrpc.proto.DemoServiceGrpcKt.DemoServiceCoroutineStub
-
-object Catalog {
-  lateinit var demoService: DemoServiceCoroutineStub
-}
