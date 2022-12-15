614ccd7fa17cda04e8426b1b097ea20c3391ad04
haliphax <haliphax@gmail.com>
2022-12-15 13:58:35 -0600

    ⬆️ Upgrade com.google.protobuf to 0.9.1

--- proto/build.gradle.kts
+++ proto/build.gradle.kts
@@ -1,9 +1,4 @@
-import com.google.protobuf.gradle.generateProtoTasks
 import com.google.protobuf.gradle.id
-import com.google.protobuf.gradle.ofSourceSet
-import com.google.protobuf.gradle.plugins
-import com.google.protobuf.gradle.protobuf
-import com.google.protobuf.gradle.protoc
 
 val grpcVersion: String by project
 val grpcKotlinVersion: String by project
@@ -32,12 +27,10 @@ protobuf {
     }
   }
 
-  generateProtoTasks {
-    ofSourceSet("main").forEach {
-      it.plugins {
-        id("grpc")
-        id("grpcKt")
-      }
+  generateProtoTasks.all().forEach {
+    it.plugins {
+      id("grpc")
+      id("grpcKt")
     }
   }
 }
